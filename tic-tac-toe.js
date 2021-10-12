5902fa70eaf35b05f8a48ef9386d148ca8e2793f
Akiel Walsh <akielwalsh@gmail.com>
2021-10-12 12:59:01 -0500

    Deleting Comments and console Logs

--- tic-tac-toe.js
+++ tic-tac-toe.js
@@ -1,4 +1,3 @@
-//"use strict"; 
 const player_X = "X";
 const player_O = "O";
 const winCond = [[0,1,2],[0,3,6],[0,4,8],[1,4,7],[2,5,8],[3,4,5],[6,7,8],[2,4,6]]
@@ -20,7 +19,6 @@ window.onload=function(){ //#1
                 element.classList.add("X"); 
                 currentPlayer = player_X;
                 board[element.id]="X";
-                //console.log(element.id)
                 winChecker();
             }  
             else if (turn%2!=0 && element.textContent == ""){//Disallow Cheating #6
@@ -28,10 +26,8 @@ window.onload=function(){ //#1
                 element.classList.add("O"); 
                 currentPlayer = player_O;
                 board[element.id]="O";
-                //console.log(element.id);
                 winChecker();
             }
-             
             turn++; 
     }) 
     element.addEventListener('mouseover',mouseOver);
@@ -50,32 +46,20 @@ function winChecker (){ //#4
     let roundWon = false;
     for (let i = 0; i <= 7; i++) {
         const winCondition = winCond[i];
-        //console.log(winCond[i])
         const a = board[winCondition[0]];
         const b = board[winCondition[1]];
         const c = board[winCondition[2]];
-        //console.log(board);
-        //console.log(currentPlayer)
         if (a === '' || b === '' || c === '') {
-            //console.log("No win")
-            //console.log(a)
-            //roundWon = true;
             continue;
-            
         }
         if (a === b && b === c) {
             roundWon = true;
-            //console.log("Win Condition Meet")
-            //console.log(a)
-            //console.log(b)
             break;
         }
-    
     }
     if (roundWon) {
         document.getElementById("status").className = "you-won"; 
         document.getElementById("status").textContent = "Congratulations! " + currentPlayer + " is the winner"
-        //isGameActive = false;
         return;
     }
 }
